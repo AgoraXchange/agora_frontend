6d50421ff63aaa67d841bc5d78fd530317ff811c
keem-hyun <hw813@naver.com>
2025-09-01 23:20:26 +0900

    refactor: remove console log on production

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Remove console logs in production
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  
   // Silence warnings
   // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  
   async rewrites() {
     return [
       {
